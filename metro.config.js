2274a92d5a285bb4e60b13495fe8e7a356bd5804
Luke Knutson <lknutson1@uchicago.edu>
2022-11-06 01:51:18 -0500

    finished userCamera and ImageData functionalities

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,24 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+module.exports = {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+    },
+    resolver: {
+      sourceExts: ['jsx', 'js', 'ts'],
+    },
+  };
