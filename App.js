d8dec1fdaa919fa6b9c2a37f2db65bbe414d9be0
lrmantovani10 <lrmantovani10@gmail.com>
2022-11-07 22:42:18 -0600

    getWeather

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import environmentalData from './functions/EnvironmentalData';
 import ImagePickerFunction from './functions/PictureFunctions';
+
 export default function mainApp() {
   return ImagePickerFunction();
 }
