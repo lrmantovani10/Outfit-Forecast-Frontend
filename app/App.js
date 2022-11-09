9ff7d6ff4c9a320784bf0d655dbe3ea653893293
ajchu28 <alliechu@gmail.com>
2022-11-08 23:59:10 -0600

    updated navigation

--- app/App.js
+++ app/App.js
@@ -1,14 +1,58 @@
 import React, { useState, useEffect } from 'react';
+import { Button, View, Text } from 'react-native';
 import environmentalData from './functions/EnvironmentalData';
 import ImagePickerFunction from './functions/PictureFunctions';
+import User from './functions/User';
 import createRootNavigator from './navigation';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
-export default function mainApp() {
-  <NavigationContainer>
-    return createRootNavigator();
-  </NavigationContainer>
-  // return environmentalData(41.795949748662835, -87.59187911021162);
-  // return ImagePickerFunction();
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {/* <Text>Home Screen</Text> */}
+      <Button
+        title="Go to Weather Functionality"
+        onPress={() => navigation.navigate('Weather')}
+      />
+    </View>
+  );
 }
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {/* <Text>Details Screen</Text> */}
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }}/>
+        <Stack.Screen name="Weather" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
+// export default function mainApp() {
+
+//   // <NavigationContainer>
+//   //   return createRootNavigator();
+//   // </NavigationContainer>
+//   // return environmentalData(41.795949748662835, -87.59187911021162);
+//   // return ImagePickerFunction();
+//   // return User("leo");
+// }
