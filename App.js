cdf28d2c5047655919c3feb4115ad610f5030196
lrmantovani10 <lrmantovani10@gmail.com>
2022-11-08 23:08:39 -0600

    dailyRecommender

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import environmentalData from './functions/EnvironmentalData';
 import ImagePickerFunction from './functions/PictureFunctions';
+import User from './functions/User';
 
 export default function mainApp() {
-  return environmentalData(41.795949748662835, -87.59187911021162);
+  return User("leo");
 }
