264eb11dcb33517cbe78c9f91c997cfa7855d08e
ajchu28 <alliechu@gmail.com>
2022-11-12 12:31:52 -0600

    functionality outlined for new functions

---
+++ app/functions/tempRanges.js
@@ -0,0 +1,7 @@
+// Goal: Add a min/max temp to clothing object with input from user
+
+// Functionality: Display a clothing item without temp range to a user and ask them to set a min/max with
+//  some type of UI (sliding scale seems like a good option)
+// Stretch Functionality: Update temp range for clothing items that already have a range
+
+// Output: Send temp range data to backend 
