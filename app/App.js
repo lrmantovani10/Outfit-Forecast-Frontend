4e50f81a764d83fda6bce73968594d76afb4fb49
Luke Contreras <contrerasl24@uchicago.edu>
2022-11-12 12:12:02 -0600

    new firebase configuration

--- app/App.js
+++ app/App.js
@@ -5,6 +5,21 @@ import ImagePickerFunction from './functions/PictureFunctions';
 import User from './functions/User';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBV58C4jXRoly73vzSV_ZofgUycqCDsEAo",
+  authDomain: "outfit-forecast.firebaseapp.com",
+  projectId: "outfit-forecast",
+  storageBucket: "outfit-forecast.appspot.com",
+  messagingSenderId: "7778456220",
+  appId: "1:7778456220:web:1776026426a9bcdda85359",
+  measurementId: "G-45F0GJK4J0"
+};
+
+if(!firebase.apps.length) {firebase.initializeApp(firebaseConfig)};
 
 function HomeScreen({ navigation }) {
   return (
--- app/functions/PictureFunctions.js
+++ app/functions/PictureFunctions.js
@@ -1,69 +1,86 @@
-import React, { useState} from 'react';
-import { Button,  View } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import * as firebase from 'firebase'
+import React, { useState, useEffect } from 'react';
+import { Button, View, Text } from 'react-native';
+import environmentalData from './functions/EnvironmentalData';
+import ImagePickerFunction from './functions/PictureFunctions';
+import User from './functions/User';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-export default function ImagePickerFunction() {
-    const [picture, update_image] = useState(null);
+const firebaseConfig = {
+  apiKey: "AIzaSyBV58C4jXRoly73vzSV_ZofgUycqCDsEAo",
+  authDomain: "outfit-forecast.firebaseapp.com",
+  projectId: "outfit-forecast",
+  storageBucket: "outfit-forecast.appspot.com",
+  messagingSenderId: "7778456220",
+  appId: "1:7778456220:web:1776026426a9bcdda85359",
+  measurementId: "G-45F0GJK4J0"
+};
 
-    const choosePicture = async () => {
-      //  Launches the image gallery. We allow cropping/other editing
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        quality: 1,
-      });
+if(!firebase.apps.length) {firebase.initializeApp(firebaseConfig)};
 
-      //  Save the picture if they successfully choose one
-      if (!result.cancelled) {
-            //upload to Firebase
-              uploadImage(result.uri, "IMAGE")
-                  .then(() => {
-                      Alert.alert("Uploaded");
-                  })
-                  .catch((error) => {
-                      Alert.alert("error");
-                  });
-      }
-    };
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {/* <Text>Camera Functionality</Text> */}
+      <Button
+        title="Go to Weather Functionality"
+        onPress={() => navigation.navigate('Weather')}
+      />
+      <Button
+        title="Go to User Functionality"
+        onPress={() => navigation.navigate('User')}
+      />
+      <Button
+        title="Go to Camera Functionality"
+        onPress={() => navigation.navigate('Camera')}
+      />
+    </View>
+  );
+}
 
-    const takePicture = async () => {
-      //  Asking the user for permission to use their camera
-      const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {/* <Text>Fetching Weather Functionality</Text> */}
+      {environmentalData(41.795949748662835, -87.59187911021162)}
+    </View>
+  );
+}
 
-      //  Exiting if they don't grant permission
-      if (permissionResult.granted === false) {
-        return;
-      }
+function WardrobeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {/* <Text>User Wardrobe Functionality</Text> */}
+      {User("leo")}
+    </View>
+  );
+}
 
-      //  Waiting to see if user successfully takes picture. If they do, save it.
-      const result = await ImagePicker.launchCameraAsync();
-      if (!result.cancelled) {
-      //upload to Firebase
-        uploadImage(result.uri, "IMAGE")
-            .then(() => {
-                Alert.alert("Uploaded");
-            })
-            .catch((error) => {
-                Alert.alert("Failed");
-            });
-      }
-    }
+function CameraScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {/* <Text>User Wardrobe Functionality</Text> */}
+      {ImagePickerFunction()}
+    </View>
+  );
+}
 
-    const uploadImage = async (uri, filename) => {
-        const response = await fetch(uri);
-        const blob = await response.blob();
-        var ref = firebase.storage().ref().child("images/" + filename);
-        return ref.put(blob);
-    }
-  
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button title="Do Survey" onPress={doSurvey} />
-        <Button title="Take Photo" onPress={takePicture} />
-        <Button title="Choose from Gallery" onPress={choosePicture} />
-      </View>
-    );
-    // You can access the currently chosen image at anytime by using the variable 'picture'.
-    // You can view the image using: {picture && <Image source={{ uri: picture }} style={{ width: 200, height: 200 }} />}
-  }
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }}/>
+        <Stack.Screen name="Camera" component={CameraScreen} options={{ title: 'Camera' }}/>
+        <Stack.Screen name="Weather" component={DetailsScreen} options={{ title: 'Weather' }}/>
+        <Stack.Screen name="User" component={WardrobeScreen} options={{ title: 'User' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
