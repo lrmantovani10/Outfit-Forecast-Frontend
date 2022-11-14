083b59ab6beed984d08cac71855ef941ae0a780b
Luke Knutson <lknutson1@uchicago.edu>
2022-11-14 11:53:00 -0600

    Added tempRanges functionality

--- app/App.js
+++ app/App.js
@@ -6,7 +6,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CustomButton from './functions/button';
 import EnvironmentalData from './functions/EnvironmentalData';
 import WardrobeGallery from './functions/wardrobeGallery'
-import TempRanges from './functions/tempRanges'
+import {TempRanges} from './functions/tempRanges'
 import ImagePickerFunction from './functions/PictureFunctions';
 import User from './functions/User';
 
@@ -92,47 +92,9 @@ function CameraScreen({ navigation }) {
 }
 
 function Preferences({ navigation }) {
-  state = {
-    modalVisible: false
-  };
-
-  setModalVisible = (visible) => {
-    this.setState({ modalVisible: visible });
-  }
-  const { modalVisible } = this.state;
   return (
-    <View style={styles.screenContainer}>
-      <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            //Alert.alert("Modal has been closed.");
-            this.setModalVisible(!modalVisible);
-          }}>
-            <View style={styles.centeredView}>
-              <View style={styles.modalView}>
-                <Text style={styles.modalText}>Hello World!</Text>
-                <Pressable
-                  style={[styles.button, styles.buttonClose]}
-                  onPress={() => this.setModalVisible(!modalVisible)}>
-                  <Text style={styles.textStyle}>Hide Modal</Text>
-                </Pressable>
-              </View>
-            </View>
-        </Modal>
-        <Pressable
-          style={[styles.button, styles.buttonOpen]}
-          onPress={() => this.setModalVisible(true)}>
-          <Text style={styles.textStyle}>Add New Temp Ranges</Text>
-        </Pressable>
-      {/* Update Temp Ranges */}
-      {/* <Button
-        title="Update Current Temp Ranges"
-        onPress={() =>
-          <tempRanges newItem={false}/>
-        }
-      /> */}
+    <View>
+      <TempRanges/>
     </View>
   );
 }
@@ -145,47 +107,6 @@ const styles = StyleSheet.create({
   },
   separator: {
     height: 15
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center"
   }
 });
 
