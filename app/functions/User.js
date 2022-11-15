1c9c558a43082b0ca63a0de5fced6f92fed0af57
lrmantovani10 <lrmantovani10@gmail.com>
2022-11-14 19:34:11 -0600

    user images

--- app/functions/User.js
+++ app/functions/User.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { View, Text, Image, ScrollView } from 'react-native';
 import styles from "./style"
