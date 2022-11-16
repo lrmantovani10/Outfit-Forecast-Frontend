1c3478bb6fb050a2db4a734eff6794851843e486
lrmantovani10 <lrmantovani10@gmail.com>
2022-11-15 23:30:32 -0600

    unit tests

--- app/functions/unitTesting.js
+++ app/functions/unitTesting.js
@@ -1,13 +1,10 @@
 
 import styles from './style';
-import { Text, View } from 'react-native';
-import environmentalData from './EnvironmentalData';
+import { Text, View, Button, Image, Platform } from 'react-native';
 import { WEATHER_KEY } from "@env"
 import { useState, useEffect } from 'react';
 import * as Location from "expo-location"
 import axios from 'axios';
-
-import { Button, Image, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 import * as firebase from 'firebase/app';
@@ -16,50 +13,17 @@ import { getStorage, ref, uploadBytes } from "firebase/storage";
 export default function unitTesting() {
 
     // States used for testing
-    const [weatherResult, setWeatherResult] = useState(["Running test 1...", "Running test 2...", "Running test 3..."])
-    const [locationTest, setLocationTest] = useState(["Running test ..."])
+    const [weatherResult1, setWeatherResult1] = useState("Running test 1...")
+    const [weatherResult2, setWeatherResult2] = useState("Running test 2...")
+    const [weatherResult3, setWeatherResult3] = useState("Running test 3...")
+    const [locationTest, setLocationTest] = useState("Running location test ...")
 
     // Unit Testing getWeather
-    const getWeather = async (lat, lon, index) => {
-        const weatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat.toString()}&lon=${lon.toString()}&appid=${WEATHER_KEY}`
-        await axios.get(weatherEndpoint).then((outcome) => {
-            const mainParameters = outcome.data.main
-            const interestingParameters = [
-                mainParameters.temp_min, mainParameters.temp_max,
-                mainParameters.feels_like, outcome.data.weather[0].main
-            ]
-            let location = outcome.data.name + ", " + outcome.data.sys.country
-            let icon = outcome.data.weather[0].icon
-            
-            let weatherCopy = [...weatherResult]
-            weatherCopy[index] = weatherTests(interestingParameters, location, icon)
-            setWeatherResult(weatherCopy)
-            
-        }).catch((error) => {
-            let weatherCopy = [...weatherResult]
-            let message
-            if (index == 2) {
-                message = "[PASS]"
-            }
-            else {
-                message = "Error fetching the weather from the API: " + error
-            }
-            weatherCopy[index] = message
-            console.log(weatherCopy[index])
-            setWeatherResult(weatherCopy)
-        })
-    }
 
     // Checking if API response meets exepceted requirements of length,
     // data type, and format
     const weatherTests = function (weather, location, icon) {
-        let message = "Testing getWeather..."
-        if (weather.length == 0) {
-            return message
-        }
-        else {
-            message = "[PASS]"
-        }
+        let message = "[PASS]"
         if (weather.length != 4) {
             message = "[FAIL] getWeather's response is not of the right size"
         }
@@ -79,11 +43,68 @@ export default function unitTesting() {
         return message
     }
 
+    const getWeather = async (lat, lon, index) => {
+        const weatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat.toString()}&lon=${lon.toString()}&appid=${WEATHER_KEY}`
+        await axios.get(weatherEndpoint).then((outcome) => {
+            const mainParameters = outcome.data.main
+            const interestingParameters = [
+                mainParameters.temp_min, mainParameters.temp_max,
+                mainParameters.feels_like, outcome.data.weather[0].main
+            ]
+            let location = outcome.data.name + ", " + outcome.data.sys.country
+            let icon = outcome.data.weather[0].icon
+            let weatherTest = weatherTests(interestingParameters, location, icon)
+            
+            if (index == 0) {
+                setWeatherResult1(weatherTest)
+            }
+            else if (index == 1) {
+                setWeatherResult2(weatherTest)
+            }
+            else {
+                setWeatherResult3(weatherTest)
+            }
+            return weatherTest
+            
+        }).catch((error) => {
+            if (index == 2) {
+                setWeatherResult3("[PASS]")
+            }
+            else {
+                if (index == 0) {
+                   setWeatherResult1("Error fetching the weather from the API: " + error)
+                }
+                else {
+                    setWeatherResult2("Error fetching the weather from the API: " + error)
+                }
+            }
+        })
+    }
 
 
+    // Unit Testing getLocation
+    const getLocation = async () => {
+        let message = "[PASS]"
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            message = "[PASS]: Permission to access the device's location was denied."
+        }
+        else {
+            let location = await Location.getCurrentPositionAsync({});
+            try {
+                location = [location.coords.latitude, location.coords.longitude]     
+                if (typeof (location[0]) != "number" || typeof (location[1]) != "number") {
+                    message = "[FAIL]: Wrong type for at least one of the coordinates"
+                }
+            }
+            catch (error) {
+                message = "[FAIL]: " + error.toString()   
+            }
+        }
+        setLocationTest(message)
+    }
 
-  
-    //Unit Testing Image Upload
+    // Unit Testing Image Upload
     const [picture, update_image] = useState(null);
 
     const takePicture = async () => {
@@ -152,53 +173,34 @@ export default function unitTesting() {
         }
     }
 
-
-    useEffect((async () => {
+    // Running tests when page loads
+    useEffect((() => {
         // Testing camera
         takePicture()
         // Inputs to getWeather
         // Two positive coordinates
-        await getWeather(20, 30, 0)
+        getWeather(20, 30, 0)
         // One of the coordinates is negative but valid
-        await getWeather(-10, 20, 1)
+        getWeather(-10, 20, 1)
         // Invalid coordinates -- should fail
-        await getWeather(-450, 100, 2)
+        getWeather(-450, 100, 2)
 
         // Location test
         // User accepts can accept or deny location access, but coordinates must 
         // remain as numbers
-        await getLocation(0)
-    
+        getLocation()
 
     }), [])
 
     return (
         <View>
-            {
-                weatherResult.map((element, index) => {
-                    return(
-                        <Text style={styles.unitText}>Weather Test { index + 1}: {
-                        element}
-                    </Text>       
-                    )
-                })
-            }
-            {
-                locationTest.map((element, index) => {
-                    return(
-                        <Text style={styles.unitText}>Location Test { index + 1}: {
-                        element}
-                    </Text>       
-                    )
-                })
-            }
-            
-            <Text style={styles.unitText}>Weather Test: {
-            weatherResult}
+            <Text style={styles.unitText}>Weather Test 1: {weatherResult1.toString()}</Text>
+            <Text style={styles.unitText}>Weather Test 2: {weatherResult2.toString()}</Text>
+            <Text style={styles.unitText}>Weather Test 3: {weatherResult3.toString()}</Text>
+            <Text style={styles.unitText}>Location Test: { locationTest.toString()} </Text>       
             <Text style={styles.unitText}>Upload Test: {
             uploadResult}
-            </Text>
-        </Text>  
+            </Text> 
         </View>
     )
 
